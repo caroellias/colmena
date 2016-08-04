f50db9cc751d69f481905f2b71f701b70397c7cb
Nomad Frost <Nomad Frost>
2016-08-04 00:57:45 -0500

    Add mapping of xbox 360 controller

---
+++ Unity/Cat320d/Assets/Scripts/MovePiston.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovePiston : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            transform.Translate(Vector3.up * (Time.deltaTime * 10));
+        }
+
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            transform.Translate(Vector3.down * (Time.deltaTime * 10));
+        }
+        //transform.Translate(Vector3.up * Time.deltaTime, Space.World);
+    }
+}
